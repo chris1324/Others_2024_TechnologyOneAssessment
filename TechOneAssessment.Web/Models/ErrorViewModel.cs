bb8b1b20392ef40fe1c4f85481aa72974d44fcb5
Chris Ng <chris.1324@hotmail.com>
2024-03-16 18:24:16 +0800

    Update

---
+++ TechOneAssessment.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TechOneAssessment.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
