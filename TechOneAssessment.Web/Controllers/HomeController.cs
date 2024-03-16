577ffe02049d623e5c5cc44ceaa56486e0d83792
Chris Ng <chris.1324@hotmail.com>
2024-03-16 19:10:13 +0800

    Update

---
+++ TechOneAssessment.Web/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TechOneAssessment.Web.Utilities;
+
+namespace TechOneAssessment.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger _logger;
+
+        public HomeController(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ConvertToWords(decimal input)
+        {
+            try
+            {
+                var result = CurrencyWordConverter.Convert(input);
+                return Content(result);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error converting dollars from numeric to words", new { input });
+                return Content("Internal Server Error");
+            }
+        }
+    }
+}
--- TechOneAssessment.Web/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace TechOneAssessment.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
