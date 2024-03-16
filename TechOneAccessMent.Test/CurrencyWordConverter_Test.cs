fcf6ab670c78092a3cc65eefdaaeaccbd62789b1
Chris Ng <chris.1324@hotmail.com>
2024-03-16 19:36:51 +0800

    Update

--- TechOneAccessMent.Test/CurrencyWordConverter_Test.cs
+++ TechOneAccessMent.Test/CurrencyWordConverter_Test.cs
@@ -25,6 +25,7 @@ namespace TechOneAccessMent.Test
         [InlineData("17.00", "SEVENTEEN DOLLARS")]
         [InlineData("18.00", "EIGHTEEN DOLLARS")]
         [InlineData("19.00", "NINETEEN DOLLARS")]
+        [InlineData("19.21", "NINETEEN DOLLARS AND TWENTY-ONE CENTS")]
         // 20.00 - 99.99 dollars
         [InlineData("20.00", "TWENTY DOLLARS")]
         [InlineData("21.00", "TWENTY-ONE DOLLARS")]
@@ -42,6 +43,10 @@ namespace TechOneAccessMent.Test
         [InlineData("81.00", "EIGHTY-ONE DOLLARS")]
         [InlineData("90.00", "NINETY DOLLARS")]
         [InlineData("91.00", "NINETY-ONE DOLLARS")]
+        [InlineData("91.21", "NINETY-ONE DOLLARS AND TWENTY-ONE CENTS")]
+        // Hundred dollars
+        [InlineData("100.00", "ONE HUNDRED DOLLARS")]
+        [InlineData("191.21", "ONE HUNDRED AND NINETY-ONE DOLLARS AND TWENTY-ONE CENTS.")]
         public void Convert(string valueAsString, string expectedResult)
         {
             // Arrange
